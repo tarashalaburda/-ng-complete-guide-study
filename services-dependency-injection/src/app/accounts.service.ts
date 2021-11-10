1a703a0523552e9d4b577edacab7d10651868192
Taras Halaburda <tarashalaburda@gmail.com>
2021-11-10 10:11:58 +0200

    add service to providers in app module

--- services-dependency-injection/src/app/accounts.service.ts
+++ services-dependency-injection/src/app/accounts.service.ts
@@ -1,3 +1,8 @@
+import { LoggingService } from './logging.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
 export class AccountsService {
   accounts = [
     {
@@ -14,8 +19,12 @@ export class AccountsService {
     }
   ];
 
+  constructor(private loggingService: LoggingService) {
+  }
+
   addAccount(name: string, status: string) {
     this.accounts.push({ name: name, status: status });
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
--- services-dependency-injection/src/app/new-account/new-account.component.ts
+++ services-dependency-injection/src/app/new-account/new-account.component.ts
@@ -6,7 +6,6 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
 })
 export class NewAccountComponent {
 
@@ -16,7 +15,5 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
