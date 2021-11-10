44fbaac9811dcf1f323e5135cfb4b128982e856e
Taras Halaburda <tarashalaburda@gmail.com>
2021-11-10 15:14:55 +0200

    added service for switching user status

--- services-dependency-injection/src/app/app.component.ts
+++ services-dependency-injection/src/app/app.component.ts
@@ -1,31 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountsService } from './accounts.service';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService]
 })
 export class AppComponent implements OnInit {
   accounts: { name: string, status: string }[] = [];
-
-  activeUsers = ['Miko', 'Poli'];
-  inactiveUsers = ['Sara', 'Tony'];
 
   constructor(private accountsService: AccountsService) {
   }
 
   ngOnInit(): void {
     this.accounts = this.accountsService.accounts;
-  }
-
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
-
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
   }
 }
